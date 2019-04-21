b4f53dce9fcba3052f05c0d6afc0acaab2875475
Kazi Mahmud <kaziraiyan@iut-dhaka.edu>
2019-04-21 12:20:15 +0600

    Bugs of HomePage fixed

---
+++ android/app/src/main/java/com/nex3z/tflitemnist/HomePage.java
@@ -0,0 +1,29 @@
+package com.nex3z.tflitemnist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomePage extends AppCompatActivity {
+
+    Button shekhaoButton,shikhoButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        shekhaoButton = findViewById(R.id.shekhaoButton);
+        shikhoButton = findViewById(R.id.shikhoButton);
+
+        shikhoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomePage.this, ShikhoActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
